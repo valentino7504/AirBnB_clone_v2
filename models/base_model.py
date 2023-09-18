979c60cbe6f586c577174fb4fea4079f4eba5569
hullaah <umaradelo1.247@gmail.com>
2023-09-18 20:03:13 +0100

    Corrected some files and implemented the amenities table

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,25 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            kwargs['updated_at'] = kwargs.get("updated")
+            kwargs['created_at'] = kwargs.get('created_at')
+            kwargs["id"] = kwargs.get("id")
+            if kwargs["created_at"]:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                kwargs["updated_at"] = datetime.now()
+            if kwargs["created_at"]:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                kwargs['created_at'] = datetime.now()
+            if not kwargs["id"]:
+                kwargs["id"] = str(uuid.uuid4())
+            if kwargs.get("__class__"):
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
--- models/place.py
+++ models/place.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, ForeignKey, Float, String
+from sqlalchemy import Column, Integer, ForeignKey, Float, String, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 from . import storage
+
+
+metadata = Base.metadata
+place_amenity = Table("place_amenity",
+                      metadata,
+                      Column("place_id", String(60), ForeignKey(
+                          "places.id"), primary_key=True),
+                      Column("amenity_id", String(60), ForeignKey(
+                          "amenities.id"), primary_key=True)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -18,8 +28,10 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenities = relationship(
+        "Amenity", secondary="place_amenity", viewonly=False)
     reviews = relationship("Review", cascade="all, delete")
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
@@ -27,4 +39,16 @@ class Place(BaseModel, Base):
             from .review import Review
             return [x for x in storage.all(
                 Review).values() if x.place_id == self.id]
+
+        @property
+        def amenities(self):
+            from .amenity import Amenity
+            return [x for x in storage.all(
+                Amenity).values() if x.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, value):
+            from .amenity import Amenity
+            if type(value) is Amenity:
+                self.amenity_ids.append(value.id)
     amenity_ids = []
