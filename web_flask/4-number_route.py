1183561c0001eb79d94a7ec1322c8dcbf070dce0
Edwin Ade <edwinmbonyjr@gmail.com>
2023-12-21 23:24:35 +0100

    Add: task 4 solution

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''
+creates a flask app with four routes
+'''
+from flask import Flask, abort
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """returns the hello message"""
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_msg():
+    """returns the 'hbnb'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_msg(text: str):
+    """returns the c messages"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>")
+def python_msg(text: str):
+    """returns the python messages"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number_msg(n):
+    """prints the number message"""
+    try:
+        value = int(n)
+    except ValueError:
+        abort(404)
+    else:
+        return f"{value} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
