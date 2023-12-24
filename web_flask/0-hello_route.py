d7824773de1cc219b2ad60d2749701df026df59c
Edwin Ade <edwinmbonyjr@gmail.com>
2023-12-24 15:05:14 +0100

    Fix: Task )

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''
 creates a flask app
+the app listens on 0.0.0.0, port 5000
 '''
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route("/", strict_slashes=False)
