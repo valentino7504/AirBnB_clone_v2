9c5f48ed4cf040bc36ac5e40ed130568b10ec190
Edwin Ade <edwinmbonyjr@gmail.com>
2023-12-24 15:06:50 +0100

    Fix: Prints the correct message in task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,13 @@ the app listens on 0.0.0.0, port 5000
 '''
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
     """returns the hello message"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
